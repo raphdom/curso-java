14affe1bd07b0d06c24a42802fab39795025b055
raphdom@gmail.com <raphdom@gmail.com>
2014-04-14 19:04:32 +0100

    exercicio 0

---
+++ src/com/cursojava/raphael/exercicio0/ExercicioMain.java
@@ -0,0 +1,13 @@
+package com.cursojava.raphael.exercicio0;
+
+/**
+ * @author Raphael Domingues
+ *
+ */
+public class ExercicioMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
