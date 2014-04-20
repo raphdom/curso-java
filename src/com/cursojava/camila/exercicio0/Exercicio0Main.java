018404e243d656a907a15763d602fb423587a9ff
camiladomingues <dra.domingues.camila@gmail.com>
2014-04-20 22:54:05 +0100

    Exercicio0

---
+++ src/com/cursojava/camila/exercicio0/Exercicio0Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cursojava.camila.exercicio0;
+
+/**
+ * @author camila
+ *
+ */
+public class Exercicio0Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println ("Hello World");	
+
+	}
+
+}
